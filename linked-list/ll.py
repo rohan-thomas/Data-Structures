8fa6e17d73b072f44bdbc0845a9e8e5056aa7d2d
rohan-thomas <rohanthomas@Rohans-MacBook-Air.local>
2017-07-25 23:16:48 -0700

    editted functions and practiced them + made a test scenario

--- linked-list/ll.py
+++ linked-list/ll.py
@@ -8,99 +8,70 @@
 #2 - we can get the data of the node
 #3 - we can get what the node points to
 #4 - we can reset what the node points to
-"""
+
 class Node(object):
-    def __init__(self, data=None, next_node=None):
+    def __init__(self,data=None,next_node=None):
         self.data = data
         self.next_node = next_node
-    
-    def get_data(self):
-        return self.data
-    
-    def get_next(self):
-        return self.next_node
-    
-    def set_next(self,new_next):
-        self.next_node = new_next
-
-
-class LinkedList(object):
-    ## Head of the list ## - top node of the list. Set to none if the list is empty
-    def __init__(self, head=None): 
-        self.head = head 
-    ## Insert ## - insert as a node at top, point to old head, new node is head
-    def insert(self,data):
-        new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-    ## Size ## - count each node by going next,next,next...
-    def size(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.get_next()
-        return count
-"""
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-class ll_node(object):
-    def __init__(self, data=None,next_node=None):
-        self.data=data
-        self.next_node= next_node
     def get_data(self):
         return self.data
     def get_next_node(self):
         return self.next_node
-    def repoint(self,new_next_node):
-        self.next_node = new_next_node
+    def point_to(self,new_next_node):
+        self.next_node=new_next_node           
 
-class ll(object):
-    def __init__(self, head=None):
+class LinkedList(object):
+    def __init__(self,head=None):
         self.head = head
-    def insert_node(self, data):
-        inserted = ll_node(data)
-        inserted.repoint(self.head)
-        self.head = inserted
-    def list_length(self):
+    def insert_node(self,data):
+        new_node = Node(data)
+        new_node.point_to(self.head)
+        self.head = new_node
+    def size_of_list(self):
         count = 0
         current = self.head
         while current:
             count += 1
             current = current.get_next_node()
         return count
-    def search_list(self, data):
-        current = self.head
+    def search_list(self,data):
         condition = False
+        current = self.head
         while current and condition is False:
             if current.get_data() == data:
                 condition = True
-            else:    
-                current = current.get_next_node
-        if current == None:
-            print("Error: No such value")
+            else:
+                current = current.get_next_node()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
     def delete_node(self,data):
         current = self.head
         condition = False
-        stored = None
+        previous = None
         while current and condition is False:
             if current.get_data() == data:
                 condition = True
-            else:
-                stored = current
+            else: 
+                previous = current
                 current = current.get_next_node()
-            if current == None:
-                print("Error: No such value")
-            if stored is None:
-                self.head = current.get_next_node
-            else:
-                stored.repoint(current.get_next_node())
-             
+        if current is None:
+            raise ValueError("Data not in list")
+        elif previous is None:
+            self.head = current.get_next_node()
+        else:
+            previous.point_to(current.get_next_node())
+
+### My messy test area
+test = Node(1)
+test2 = LinkedList()
+test3 = test2.insert_node(test)
+print(test.get_data())
+print(test2)
+print(test2.size_of_list())
+tom = Node(2)
+test2.insert_node(tom)
+print("tom is",tom.get_data())
+print(test2.size_of_list())
+test2.delete_node(tom)
+print(test2.size_of_list())
